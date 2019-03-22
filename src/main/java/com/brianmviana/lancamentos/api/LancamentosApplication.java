58ff7400c4c6874b3bc9dd4d289fc2fffd333573
Brian M. Viana <brianmviana@gmail.com>
2019-03-22 20:40:29 -0300

    Configurando Spring Security OAuth2 com token e cookie

--- src/main/java/com/brianmviana/lancamentos/api/LancamentosApplication.java
+++ src/main/java/com/brianmviana/lancamentos/api/LancamentosApplication.java
@@ -9,5 +9,14 @@ public class LancamentosApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LancamentosApplication.class, args);
 	}
-
+	
+//	@Bean
+//	public WebMvcConfigurer corsConfigurer() {
+//		return new WebMvcConfigurerAdapter() {
+//			@Override
+//			public void addCorsMappings(CorsRegistry registry) {
+//				registry.addMapping("/*").allowedOrigins("URL_DE_ORIGEM");
+//			}
+//		};
+//	}
 }
